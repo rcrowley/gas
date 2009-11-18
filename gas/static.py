6f73b395ca82f3f7941a9e1a25e76aa751ee3782
Richard Crowley <r@rcrowley.org>
2009-11-18 19:13:11 +0000

    Added rest-tastic error responses and chaining of GET parameters into a chain of generators.  Implemented grep and stubbed awk and sed.

--- gas/static.py
+++ gas/static.py
@@ -2,10 +2,13 @@
 Static Gas
 Richard Crowley <richard@opendns.com>
 
-A WSGI app for streaming static files.
+A WSGI app for streaming static files through grep-, awk- and sed-like
+generators.
 """
 
+import cgi
 import os
+import re
 
 class Static(object):
     """
@@ -21,18 +24,63 @@ class Static(object):
         self.document_root = document_root
 
     def __call__(self, environ, start_response):
-        pathname = os.path.join(
-            self.document_root,
-            environ["PATH_INFO"][1:]
-        )
-        if os.path.exists(pathname):
-            start_response("200 OK", [
+        pathname = os.path.join(self.document_root, environ["PATH_INFO"][1:])
+
+        if "GET" != environ["REQUEST_METHOD"]:
+            start_response("405 Method Not Allowed", [
                 ("Content-Type", "text/plain"),
+                ("Content-Length", 0),
             ])
-            return open(pathname)
-        else:
+            return []
+
+        if not os.path.exists(pathname):
             start_response("404 Not Found", [
                 ("Content-Type", "text/plain"),
                 ("Content-Length", 0),
             ])
             return []
+
+        try:
+            fd = open(pathname)
+        except IOError:
+            start_response("500 Internal Server Error", [
+                ("Content-Type", "text/plain"),
+                ("Content-Length", 0),
+            ])
+            return []
+        start_response("200 OK", [
+            ("Content-Type", "text/plain"),
+        ])
+
+        if "QUERY_STRING" not in environ:
+            return fd
+
+        # Chain available utilities in the order they appear in QUERY_STRING
+        params = cgi.parse_qsl(environ["QUERY_STRING"])
+        for param in params:
+            if hasattr(self, param[0]):
+                fd = getattr(self, param[0])(fd, param[1])
+        return fd
+
+    def grep(self, fd, pattern):
+        """
+        Yield all lines of the given iterable that match the given pattern.
+        """
+        regex = re.compile(pattern)
+        for line in fd:
+            if regex.search(line):
+                yield line
+
+    def awk(self, fd, fields):
+        """
+        TODO
+        """
+        for line in fd:
+            yield line
+
+    def sed(self, fd, expr):
+        """
+        TODO
+        """
+        for line in fd:
+            yield line
